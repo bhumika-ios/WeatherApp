ef342020dd0bb5b91ba3f3a4b8785a4949bd864e
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-03-07 18:02:13 +0530

    create weather model and weather viewmodel

---
+++ WeatherApp/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Bhumika Patel on 07/03/24.
+//
+
+import Foundation
---
+++ WeatherApp/WeatherViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by Bhumika Patel on 07/03/24.
+//
+
+import SwiftUI
+
+struct WeatherViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherViewModel()
+}
