5a40ab438a5bcc00c534e2de203834db6cab290c
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-03-07 17:52:47 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Bhumika Patel on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
